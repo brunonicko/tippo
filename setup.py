3becbc02c7b4271efc174675ad35939d158c47ef
Bruno Nicko <brunonicko@gmail.com>
2022-07-13 09:36:20 -0700

    Fix requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+import os
 import setuptools  # type: ignore
 
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    install_requires = [line.strip(os.linesep) for line in fh.readlines()]
+
+
 setuptools.setup(
     name="tippo",
-    version="1.0.0",
+    version="1.0.1",
     author="Bruno Nicko",
     author_email="brunonicko@gmail.com",
     description="Cleaner and forwards compatible way to use typing features.",
@@ -15,7 +20,7 @@ setuptools.setup(
     url="https://github.com/brunonicko/tippo",
     packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
     package_data={"tippo": ["py.typed"]},
-    install_requires=["typing_extensions"],
+    install_requires=install_requires,
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
@@ -27,9 +32,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    python_requires=(
-        ">= 2.7, "
-        "!= 3.0.*, != 3.1.*, != 3.2.*, != 3.3.*, != 3.4.*, != 3.5.*, != 3.6.*"
-    ),
+    python_requires=(">= 2.7, " "!= 3.0.*, != 3.1.*, != 3.2.*, != 3.3.*, != 3.4.*, != 3.5.*, != 3.6.*"),
     tests_require=["pytest"],
 )
